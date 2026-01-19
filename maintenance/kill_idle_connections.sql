094a6e6599af561b5af2b76ce3a0f1cadcf193a1
gudcf <resendegcr@icloud.com>
2026-01-19 00:35:13 -0300

    Delete maintenance/kill_idle_connections.sql

--- maintenance/kill_idle_connections.sql
+++
@@ -1,8 +0,0 @@
--- 🚑 Derrubar conexões 'idle' (ociosas) que estão travando o banco
--- CUIDADO: Use apenas em caso de emergência quando o pool de conexões estourar
-
-SELECT pg_terminate_backend(pid)
-FROM pg_stat_activity
-WHERE state = 'idle'
-AND state_change < current_timestamp - INTERVAL '30 minutes'
-AND pid <> pg_backend_pid(); -- Não se matar
